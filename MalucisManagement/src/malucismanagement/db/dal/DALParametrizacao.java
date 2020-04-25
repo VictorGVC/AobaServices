c897b91e04346c318a7c216e258832998cfca53b
Mateus <Mateus@>
2020-04-24 21:10:38 -0300

    - Entidade e DAL de Parametrização criados

---
+++ MalucisManagement/src/malucismanagement/db/dal/DALParametrizacao.java
@@ -0,0 +1,41 @@
+package malucismanagement.db.dal;
+
+import java.sql.PreparedStatement;
+import malucismanagement.db.banco.Banco;
+import malucismanagement.db.entidades.Parametrizacao;
+
+public class DALParametrizacao {
+    
+    public boolean gravar(Parametrizacao p) {
+        
+        String sql = "INSERT INTO Parametrizacao() VALUES()";
+        
+        return Banco.getCon().manipular(sql);
+    }
+    
+    public boolean alterar(Parametrizacao p) {
+        
+        String sql = "UPDATE Parametrizacao SET";
+        
+        return Banco.getCon().manipular(sql);
+    }
+    
+    public boolean gravarFoto(Parametrizacao p) {
+        
+        try {
+            
+            PreparedStatement st = Banco.con.getConnect().prepareStatement("UPDATE Parametrizacao SET par_foto = ?");
+            
+            st.setBytes(1, p.getLogo());
+            st.execute();
+            
+            return true;
+           
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+        
+        return false;
+    }
+}
