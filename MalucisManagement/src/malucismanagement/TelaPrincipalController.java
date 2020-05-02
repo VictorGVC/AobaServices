790acce81750504b1f2e1b7a3f54fb040e243c83
Victor Gabriel Viana <vicgabriel17@hotmail.com>
2020-05-02 15:21:05 -0300

    - Tela gerenciamento de funcionários criada

--- MalucisManagement/src/malucismanagement/TelaPrincipalController.java
+++ MalucisManagement/src/malucismanagement/TelaPrincipalController.java
@@ -67,7 +67,7 @@ public class TelaPrincipalController implements Initializable {
         
         try {
             
-            Parent root = FXMLLoader.load(getClass().getResource("TelaClientes.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("TelaLogin_Cadastro.fxml"));
             efeito(true);
             pnprincipal.setCenter(root);
         }
