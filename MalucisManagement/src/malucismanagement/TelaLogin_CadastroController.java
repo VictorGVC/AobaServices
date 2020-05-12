e0f01792025e59eab5a40f1de7896c505c38b988
Mateus <Mateus@>
2020-05-11 23:56:15 -0300

    - Programa fechando totalmente

--- MalucisManagement/src/malucismanagement/TelaLogin_CadastroController.java
+++ MalucisManagement/src/malucismanagement/TelaLogin_CadastroController.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.animation.FadeTransition;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -21,6 +23,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.util.Duration;
 import malucismanagement.db.dal.DALFuncionario;
 import malucismanagement.db.dal.DALParametrizacao;
@@ -154,8 +157,8 @@ public class TelaLogin_CadastroController implements Initializable {
         }
     }
     
-    private void chamaPrincipal() throws IOException
-    {
+    private void chamaPrincipal() throws IOException {
+        
         Parent root = FXMLLoader.load(getClass().getResource("TelaPrincipal.fxml"));
         Scene scene = new Scene(root);
         Stage stage = new Stage();
@@ -165,5 +168,16 @@ public class TelaLogin_CadastroController implements Initializable {
         stage.setTitle("Papelaria Maluci");
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            
+            @Override
+            public void handle(WindowEvent t) {
+                
+                t.consume();
+                stage.close();
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 }
