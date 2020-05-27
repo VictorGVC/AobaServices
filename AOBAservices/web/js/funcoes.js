d736503607ba77b4b073a89339f1436a8fcb0a15
jataf <jataf@DESKTOP-BDLKIAK>
2020-05-27 18:54:03 -0300

    - Entidades e DAls criadas de anuncio,categoria,servicos e foto

--- AOBAservices/web/js/funcoes.js
+++ AOBAservices/web/js/funcoes.js
@@ -4,3 +4,45 @@ var modal = document.getElementById('id01');
             modal.style.display = "none";
     }
 }
+
+function PreencheCBCategoria(){
+    
+    var cbcategoria = document.getElementById("categoria");
+    
+    while (cbcategoria.length > 0) 
+        cbcategoria.remove(0);
+
+    for (var i = 0; i < Variaveldobanco.length; i++) {
+        var elem = document.createElement('tag')
+        elem.text  = Variaveldobanco.Descricao;
+    
+        cbcategoria.add(elem, cbcategoria.options[i]);
+    }
+    
+}
+
+function PreencheCBServicos(){
+    
+    var cbservicos = document.getElementById("servicos");
+    
+    while (cbservicos.length > 0) 
+        cbservicos.remove(0);
+
+    for (var i = 0; i < Variaveldobanco.length; i++) {
+        var elem = document.createElement('tag')
+        elem.text  = Variaveldobanco.Descricao;
+    
+        cbservicos.add(elem, cbservicos.options[i]);
+    }
+    
+}
+
+function SalvarAnuncio(){
+    
+}
+
+function SalvarPrestador(){
+    
+    
+    
+}
