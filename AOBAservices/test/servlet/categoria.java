3d04dddbfcefca237f9d7733f5a7abc45466c750
Mateus <Mateus@>
2020-06-21 09:57:28 -0300

    - Mesclagem

--- AOBAservices/test/servlet/categoria.java
+++ AOBAservices/test/servlet/categoria.java
@@ -2,15 +2,14 @@ package servlet;
 
 import dal.DALCategoria;
 import entidades.Categoria;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "categoria", urlPatterns = {"/categoria"})
 public class categoria extends HttpServlet {
